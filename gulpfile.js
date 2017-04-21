b77e1675ccd8769bf22edfb5ee74bf23bbca9e62
Danang Widiantoro <danang@fisip.net>
2017-04-22 05:25:35 +0700

    include copy files

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,16 @@ var gutil = require('gulp-util');
 var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
 
+var uglify = require('gulp-uglify');
+var Packer = require('./packer');
+var streamify = require('gulp-streamify');
+
 // Sass Requires
-var sass = require('gulp-sass');                // Include SASS
-var autoprefix = require('gulp-autoprefixer');  // Autoprefixer (always)
-var rename = require('gulp-rename');            // Gulp Rename
-var sourcemaps = require('gulp-sourcemaps');    // Sourcemaps (for sass)
-var merge = require('merge2');    							// merging multi src
+var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var merge = require('merge2');
 
 var config = require('./config');
 var dest = (typeof config.dest.path=="undefined") ? "dest" : (config.dest.path=="" ? "dest" : config.dest.path);
@@ -64,35 +68,73 @@ gulp.task('scripts', function(cb)
 	var fileName = config.dest.js||'script.js';
 	if (config.js.length > 0)
 	{
-		var compresor;
 		switch(config.jscompress) {
-			case 3:
-				var uinline = require('gulp-uglify-inline');
-				compresor = uinline();
-				break;
 			case 2:
-				var packer = require('gulp-packer')
-		  	var streamify = require('gulp-streamify')
-				compresor = streamify(packer({base62: true, shrink: true}))
+		  	return gulp.src(config.js)
+					.pipe(concat(fileName))
+					.pipe(uglify({mangle: false, compress: true }))
+					.pipe(streamify(Packer({base62: true, shrink: true})))
+					.pipe(gulp.dest(gulpDest));
 				break;
 			default:
-				var uglify = require('gulp-uglify');
-				compresor = uglify({mangle: true, compress: true, eval: true });
+				return gulp.src(config.js)
+					.pipe(concat(fileName))
+					.pipe(uglify({mangle: true, compress: true }))
+					.pipe(gulp.dest(gulpDest));
+				break;
 		}
-
-		return gulp.src(config.js)
-		.pipe(concat(fileName))
-		.pipe(compresor)
-		.pipe(gulp.dest(gulpDest));
 	}
 	return false;
 });
-
 gulp.task('copy', function()
 {
+	function exec(files, dest, name=null)
+	{
+		if (name)
+		{
+			var doMinify = false;
+			if (typeof files=="string")
+			{
+				var match = /\.([^\.]+)$/.exec(files);
+				if (match!=null) {
+					switch(match[1]) {
+						case 'js':
+								doMinify = true;
+								gulp.src(files)
+								.pipe(concat(name))
+								.pipe(uglify({mangle: false, compress: true }))
+								.pipe(streamify(Packer({base62: true, shrink: true})))
+								.pipe(gulp.dest(dest));
+							break;
+						case 'scss':
+								doMinify = true;
+								gulp.src(files)
+								.pipe(concat(name))
+								.pipe(sass())
+								.pipe(cssmin())
+								.pipe(gulp.dest(dest));
+							break;
+						case 'css':
+								doMinify = true;
+								gulp.src(files)
+								.pipe(concat(name))
+								.pipe(cssmin())
+								.pipe(gulp.dest(dest));
+							break;
+					}
+				}
+			}
+			if (!doMinify)
+			{
+				gulp.src(file).pipe(concat(name)).pipe(gulp.dest(dest));
+			}
+		}else{
+			gulp.src(file).pipe(gulp.dest(dest));
+		}
+	}
 	if (typeof config.copy!="undefined")
 	{
-		var gulpDest;
+		var gulpDest, file, src, match;
 		for (var dir in config.copy)
 		{
 			if (!config.copy.hasOwnProperty(dir)) continue;
@@ -100,16 +142,15 @@ gulp.task('copy', function()
 			for(var i in config.copy[dir])
 			{
 				if (!config.copy[dir].hasOwnProperty(i)) continue;
-				if (typeof config.copy[dir][i]=="string")
+				file = config.copy[dir][i];
+				if (typeof file=="string")
 				{
-					// console.log([config.copy[dir][i], gulpDest]);
-					gulp.src(config.copy[dir][i]).pipe(gulp.dest(gulpDest));
+					exec(file, gulpDest);
 				}else{
-					for(var j in config.copy[dir][i])
+					for(var name in config.copy[dir][i])
 					{
-						if (!config.copy[dir][i].hasOwnProperty(j)) continue;
-						// console.log([config.copy[dir][i][j], j]);
-						gulp.src(config.copy[dir][i][j]).pipe(concat(j)).pipe(gulp.dest(gulpDest));
+						if (!config.copy[dir][i].hasOwnProperty(name)) continue;
+						exec(config.copy[dir][i][name], gulpDest, name);
 					}
 				}
 			}
