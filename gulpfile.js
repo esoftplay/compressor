711d7ba210b50651837324b38a9998000a82e730
Danang Widiantoro <danang@fisip.net>
2017-04-28 01:53:16 +0700

    refine file map to compress

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var Packer = require('./packer');
 var streamify = require('gulp-streamify');
+var pump = require('pump');
 
 var sass = require('gulp-sass');
 var autoprefix = require('gulp-autoprefixer');
@@ -17,146 +18,132 @@ var merge = require('merge2');
 var config = require('./config');
 var dest = (typeof config.dest.path=="undefined") ? "dest" : (config.dest.path=="" ? "dest" : config.dest.path);
 
-
-
-gulp.task('styles', function()
-{
-	var gulpDest = dest+"/css";
-	var fileName = config.dest.css||'style.css';
-	if (config.css.length > 0 && config.scss.length > 0)
-	{
-		var cssStream = gulp.src(config.css).pipe(concat('css-files.css'));
-		var scssStream = gulp.src(config.scss).pipe(sass()).pipe(concat('scss-files.css'));
-		return merge(cssStream, scssStream)
-		.pipe(concat(fileName))
-		.pipe(cssmin())
-		.pipe(gulp.dest(gulpDest));
-	}else
-	if (config.css.length > 0)
-	{
-		return gulp.src(config.css)
-		.pipe(concat(fileName))
-		.pipe(cssmin())
-		.pipe(gulp.dest(gulpDest));
-	}else
-	if (config.scss.length > 0)
-	{
-		return gulp.src(config.css)
-		.pipe(concat(fileName))
-		.pipe(sass())
-		.pipe(cssmin())
-		.pipe(gulp.dest(gulpDest));
+var getExt = function(filename) {
+	var match = /\.([^\.]+)$/.exec(filename);
+	if (match) {
+		return match[1].toLowerCase();
 	}
-	return false;
-});
-
-gulp.task('fonts', function()
-{
-	var gulpDest = dest+"/fonts";
-	if (config.font.length > 0)
-	{
-		return gulp.src(config.font).pipe(gulp.dest(gulpDest));
-	}
-	return false;
-});
-
-gulp.task('scripts', function(cb)
-{
-	var gulpDest = dest+"/js";
-	var fileName = config.dest.js||'script.js';
-	if (config.js.length > 0)
-	{
-		switch(config.jscompress) {
-			case 2:
-		  	return gulp.src(config.js)
-					.pipe(concat(fileName))
-					.pipe(uglify({mangle: false, compress: true }))
-					.pipe(streamify(Packer({base62: true, shrink: true})))
-					.pipe(gulp.dest(gulpDest));
-				break;
-			default:
-				return gulp.src(config.js)
-					.pipe(concat(fileName))
-					.pipe(uglify({mangle: true, compress: true }))
-					.pipe(gulp.dest(gulpDest));
-				break;
+	return "none";
+};
+if (typeof config.source!="undefined" && config.source!="") {
+	var fixPath = function(file) {
+		if (file.substr(0, 1)=="/" || file.substr(0, 2)=="./") {
+			return file;
+		// }else
+		// if (/^(?:f|ht)tps?:\/\//i.test(file)) {
+		// 	var ext = getExt(file);
+		// 	return request(file).pipe(writeStream('temp.'+ext));
 		}
-	}
-	return false;
-});
-gulp.task('copy', function()
-{
-	function exec(files, dest, name=null)
-	{
-		if (name)
-		{
-			var doMinify = false;
-			if (typeof files=="string")
-			{
-				var match = /\.([^\.]+)$/.exec(files);
-				if (match!=null) {
-					switch(match[1]) {
-						case 'js':
-								doMinify = true;
-								gulp.src(files)
-								.pipe(concat(name))
-								.pipe(uglify({mangle: false, compress: true }))
-								.pipe(streamify(Packer({base62: true, shrink: true})))
-								.pipe(gulp.dest(dest));
-							break;
-						case 'scss':
-								doMinify = true;
-								gulp.src(files)
-								.pipe(concat(name))
-								.pipe(sass())
-								.pipe(cssmin())
-								.pipe(gulp.dest(dest));
-							break;
-						case 'css':
-								doMinify = true;
-								gulp.src(files)
-								.pipe(concat(name))
-								.pipe(cssmin())
-								.pipe(gulp.dest(dest));
-							break;
-					}
-				}
-			}
-			if (!doMinify)
-			{
-				gulp.src(file).pipe(concat(name)).pipe(gulp.dest(dest));
+		return config.source+file;
+	};
+	var loops = ["css", "scss", "font", "js"];
+	for (var i = 0; i < loops.length; i++) {
+		if (config[loops[i]]) {
+			for (var j = 0; j < config[loops[i]].length; j++) {
+				config[loops[i]][j] = fixPath(config[loops[i]][j]);
 			}
 		}else{
-			gulp.src(file).pipe(gulp.dest(dest));
+			config[loops[i]] = [];
 		}
 	}
-	if (typeof config.copy!="undefined")
-	{
-		var gulpDest, file, src, match;
-		for (var dir in config.copy)
-		{
-			if (!config.copy.hasOwnProperty(dir)) continue;
-			gulpDest = dest+dir;
-			for(var i in config.copy[dir])
-			{
-				if (!config.copy[dir].hasOwnProperty(i)) continue;
-				file = config.copy[dir][i];
-				if (typeof file=="string")
-				{
-					exec(file, gulpDest);
-				}else{
-					for(var name in config.copy[dir][i])
-					{
-						if (!config.copy[dir][i].hasOwnProperty(name)) continue;
-						exec(config.copy[dir][i][name], gulpDest, name);
+	if (config.copy) {
+		for(var path in config.copy) {
+			for (var file in config.copy[path]) {
+				if (typeof config.copy[path][file]=="object") {
+					for (var name in config.copy[path][file]) {
+						config.copy[path][file][name] = fixPath(config.copy[path][file][name]);
 					}
+				}else{
+					config.copy[path][file] = fixPath(config.copy[path][file]);
+				}
+			}
+		}
+	}else{
+		config.copy = {};
+	}
+}
+var Execute = function(cb, files, dest, type, name=null) {
+	if (name) {
+		var tasks = [files];
+		tasks.push(concat(name));
+		switch(type) {
+			case 'css':
+				tasks.push(cssmin());
+				break;
+			case 'scss':
+				tasks.push(sass());
+				tasks.push(cssmin());
+				break;
+			case 'js':
+				if (config.jscompress) {
+					tasks.push(uglify({mangle: false, compress: true }));
+					switch(config.jscompress) {
+						case 2:
+							tasks.push(streamify(Packer({base62: true, shrink: true})));
+							break;
+					}
+				}
+				break;
+		}
+		tasks.push(gulp.dest(dest));
+		pump(tasks);
+	}else{
+		files.pipe(gulp.dest(dest));
+	}
+	return true;
+};
+gulp.task('styles', function(cb) {
+	var gulpDest = dest+"/css";
+	var fileName = config.dest.css||'style.css';
+	if (config.css.length > 0 && config.scss.length > 0) {
+		var cssStream = gulp.src(config.css).pipe(concat('css-files.css'));
+		var scssStream = gulp.src(config.scss).pipe(sass()).pipe(concat('scss-files.css'));
+		return Execute(cb, merge(cssStream, scssStream), gulpDest, "css", fileName);
+	}else
+	if (config.css.length > 0) {
+		return Execute(cb, gulp.src(config.css), gulpDest, "css", fileName);
+	}else
+	if (config.scss.length > 0) {
+		return Execute(cb, gulp.src(config.scss), gulpDest, "scss", fileName);
+	}
+	return false;
+});
+
+gulp.task('fonts', function(cb) {
+	var gulpDest = dest+"/fonts";
+	if (config.font.length > 0) {
+		return Execute(cb, gulp.src(config.font), gulpDest, "font");
+	}
+	return false;
+});
+gulp.task('scripts', function(cb) {
+	var gulpDest = dest+"/js";
+	var fileName = config.dest.js||'script.js';
+	if (config.js.length > 0) {
+		Execute(cb, gulp.src(config.js), gulpDest, "js", fileName);
+	}
+	return false;
+});
+gulp.task('copy', function(cb) {
+	var gulpDest, file, src, match;
+	for (var dir in config.copy) {
+		if (!config.copy.hasOwnProperty(dir)) continue;
+		gulpDest = dest+dir;
+		for(var i in config.copy[dir]) {
+			if (!config.copy[dir].hasOwnProperty(i)) continue;
+			file = config.copy[dir][i];
+			if (typeof file=="string") {
+				Execute(cb, gulp.src(file), gulpDest, getExt(file));
+			}else{
+				for(var name in file) {
+					if (!file.hasOwnProperty(name)) continue;
+					Execute(cb, gulp.src(file[name]), gulpDest, getExt(file[name]), name);
 				}
 			}
 		}
 	}
 	return false;
 });
-
 gulp.task('watch', function() {
 	gulp.watch(config.css, ['styles']);
 	gulp.watch(config.scss, ['styles']);
--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
-how to use it in 9 steps:
+how to use it in 10 steps:
 
 1. install npm (Node Package Manager) = [https://docs.npmjs.com/getting-started/installing-node](Link URL)
 1. install gulp -> `$ npm install --save gulp-install`
 1. change directory to this doc_root
 1. execute `$ npm install`
 1. execute `$ npm install gulp-packer@0.1.1 --save`
+1. execute `$ npm install jsdom@9.12.0 --save`
 1. execute `$ npm rebuild node-sass`
 1. create file named 'config.js' like 'config.js.sample'
-1. specify all files and destination in 'config.js'
+1. specify all files and path in 'config.js'
 1. execute command `$ gulp`
